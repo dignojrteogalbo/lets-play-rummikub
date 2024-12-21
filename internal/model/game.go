a9d88f5efdb422f26b3573acdc32a4894a5f9d4c
dignojrteogalbo <dignojrteogalbo@gmail.com>
2024-12-21 20:44:25 +0800

    Added test for NewGame

--- internal/model/game.go
+++ internal/model/game.go
@@ -1,17 +1,6 @@
 package model
 
-const (
-	ValueJoker uint8 = iota
-	ColorBlack Color = iota
-	ColorBlue
-	ColorRed
-	ColorGreen
-)
-
 type (
-	Color uint8
-	Value uint8
-
 	Game interface {
 		NextTurn()
 		PlayMove()
@@ -21,66 +10,7 @@ type (
 	gameInstance struct {
 		tiles [106]Piece
 	}
-
-	Piece interface {
-		IsJoker() bool
-		IsSameColor(Piece) bool
-		IsSameValue(Piece) bool
-	}
-
-	piece struct {
-		Value uint8
-		Color
-	}
 )
-
-func NewPiece(v uint8, c Color) Piece {
-	if v > 13 {
-		return nil
-	}
-	if c > ColorGreen {
-		return nil
-	}
-	return &piece{v, c}
-}
-
-func isValidPiece(p Piece) bool {
-	if p == nil {
-		return false
-	}
-	piece, ok := p.(*piece)
-	if !ok {
-		return false
-	}
-	if piece.Value > 13 {
-		return false
-	}
-	if piece.Color > ColorGreen {
-		return false
-	}
-	return true
-}
-
-func (p *piece) IsJoker() bool {
-	if isValidPiece(p) {
-		return p.Value == 0
-	}
-	return false
-}
-
-func (p *piece) IsSameColor(compare Piece) bool {
-	if isValidPiece(p) && isValidPiece(compare) {
-		return p.Color == compare.(*piece).Color
-	}
-	return false
-}
-
-func (p *piece) IsSameValue(compare Piece) bool {
-	if isValidPiece(p) && isValidPiece(compare) {
-		return p.Value == compare.(*piece).Value
-	}
-	return false
-}
 
 func NewGame() Game {
 	instance := new(gameInstance)
--- internal/model/game_test.go
+++ internal/model/game_test.go
@@ -1,125 +1,31 @@
 package model
 
 import (
-	"github.com/stretchr/testify/assert"
-
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestNewPiece(t *testing.T) {
-	t.Run("ShouldReturnBlackJokerPiece", func(t *testing.T) {
-		newPiece := NewPiece(0, ColorBlack)
-		result, ok := newPiece.(*piece)
-		if assert.True(t, ok) {
-			assert.Equal(t, result.Value, uint8(0))
-			assert.Equal(t, result.Color, ColorBlack)
-		}
-	})
-	t.Run("ShouldReturnBlueOnePiece", func(t *testing.T) {
-		newPiece := NewPiece(1, ColorBlue)
-		result, ok := newPiece.(*piece)
-		if assert.True(t, ok) {
-			assert.Equal(t, result.Value, uint8(1))
-			assert.Equal(t, result.Color, ColorBlue)
-		}
-	})
-	t.Run("ShouldReturnRedThirteenPiece", func(t *testing.T) {
-		newPiece := NewPiece(13, ColorRed)
-		result, ok := newPiece.(*piece)
-		if assert.True(t, ok) {
-			assert.Equal(t, result.Value, uint8(13))
-			assert.Equal(t, result.Color, ColorRed)
-		}
-	})
-	t.Run("ShouldReturnNilOnInvalidValue", func(t *testing.T) {
-		newPiece := NewPiece(14, ColorRed)
-		assert.Nil(t, newPiece)
-	})
-	t.Run("ShouldReturnNilOnInvalidColor", func(t *testing.T) {
-		newPiece := NewPiece(5, 14)
-		assert.Nil(t, newPiece)
-	})
+func createColorTiles(color Color) []Piece {
+	tiles := make([]Piece, 0)
+	for value := uint8(1); value <= uint8(13); value++ {
+		tiles = append(tiles, NewPiece(value, color))
+		tiles = append(tiles, NewPiece(value, color))
+	}
+	return tiles
 }
 
-func TestIsValidPiece(t *testing.T) {
-	t.Run("ShouldReturnTrueOnValidPiece", func(t *testing.T) {
-		newPiece := NewPiece(5, ColorBlue)
-		valid := isValidPiece(newPiece)
-		assert.True(t, valid)
-	})
-	t.Run("ShouldReturnFalseOnNilPiece", func(t *testing.T) {
-		newPiece := (Piece)(nil)
-		valid := isValidPiece(newPiece)
-		assert.False(t, valid)
-	})
-	t.Run("ShouldReturnFalseOnFailedTypeAssertion", func(t *testing.T) {
-		mockPiece := &struct{ Piece }{}
-		valid := isValidPiece(mockPiece)
-		assert.False(t, valid)
-	})
-	t.Run("ShouldReturnFalseOnInvalidValue", func(t *testing.T) {
-		newPiece := &piece{14, ColorBlack}
-		valid := isValidPiece(newPiece)
-		assert.False(t, valid)
-	})
-	t.Run("ShouldReturnFalseOnInvalidColor", func(t *testing.T) {
-		newPiece := &piece{0, 5}
-		valid := isValidPiece(newPiece)
-		assert.False(t, valid)
-	})
-}
-
-func TestIsJoker(t *testing.T) {
-	t.Run("ShouldReturnTrueOnJoker", func(t *testing.T) {
-		joker := NewPiece(ValueJoker, ColorBlack)
-		assert.True(t, joker.IsJoker())
-	})
-	t.Run("ShouldReturnFalseOnNotJoker", func(t *testing.T) {
-		notJoker := NewPiece(11, ColorBlue)
-		assert.False(t, notJoker.IsJoker())
-	})
-	t.Run("ShouldReturnFalseOnInvalidPiece", func(t *testing.T) {
-		invalidPiece := &piece{14, ColorBlack}
-		assert.False(t, invalidPiece.IsJoker())
-	})
-}
-
-func TestIsSameColor(t *testing.T) {
-	t.Run("ShouldReturnTrueOnSameColorPieces", func(t *testing.T) {
-		blackOne := NewPiece(1, ColorBlack)
-		blackThree := NewPiece(3, ColorBlack)
-		assert.True(t, blackOne.IsSameColor(blackThree))
-		assert.True(t, blackThree.IsSameColor(blackOne))
-	})
-	t.Run("ShouldReturnFalseOnDifferentColorPieces", func(t *testing.T) {
-		blackOne := NewPiece(1, ColorBlack)
-		blueThree := NewPiece(3, ColorBlue)
-		assert.False(t, blackOne.IsSameColor(blueThree))
-		assert.False(t, blueThree.IsSameColor(blackOne))
-	})
-	t.Run("ShouldReturnFalseOnInvalidPieces", func(t *testing.T) {
-		invalidPiece := Piece(nil)
-		redFive := NewPiece(5, ColorRed)
-		assert.False(t, redFive.IsSameColor(invalidPiece))
-	})
-}
-
-func TestIsSameValue(t *testing.T) {
-	t.Run("ShouldReturnTrueOnSameValuePieces", func(t *testing.T) {
-		blackOne := NewPiece(1, ColorBlack)
-		redOne := NewPiece(1, ColorRed)
-		assert.True(t, blackOne.IsSameValue(redOne))
-		assert.True(t, redOne.IsSameValue(blackOne))
-	})
-	t.Run("ShouldReturnFalseOnDifferentValuePieces", func(t *testing.T) {
-		blackOne := NewPiece(1, ColorBlack)
-		blueThree := NewPiece(3, ColorBlue)
-		assert.False(t, blackOne.IsSameValue(blueThree))
-		assert.False(t, blueThree.IsSameValue(blackOne))
-	})
-	t.Run("ShouldReturnFalseOnInvalidPieces", func(t *testing.T) {
-		invalidPiece := Piece(nil)
-		redFive := NewPiece(5, ColorRed)
-		assert.False(t, redFive.IsSameValue(invalidPiece))
-	})
+func TestNewGame(t *testing.T) {
+	expectedTiles := make([]Piece, 0)
+	expectedTiles = append(expectedTiles, NewPiece(ValueJoker, ColorBlack), NewPiece(ValueJoker, ColorBlack))
+	expectedTiles = append(expectedTiles, createColorTiles(ColorBlack)...)
+	expectedTiles = append(expectedTiles, createColorTiles(ColorBlue)...)
+	expectedTiles = append(expectedTiles, createColorTiles(ColorRed)...)
+	expectedTiles = append(expectedTiles, createColorTiles(ColorGreen)...)
+	game := NewGame()
+	if assert.NotNil(t, game) {
+		gameStruct, ok := game.(*gameInstance)
+		assert.True(t, ok)
+		assert.ElementsMatch(t, gameStruct.tiles, expectedTiles)
+	}
 }
